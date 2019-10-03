743f06bee1027b488487e40cd3cb26535c42de4e
NICOLCH <christoper.nicolas@oocl.com>
2019-10-03 09:42:54 +0800

    Will follow up the editing later

--- js/todo.js
+++ js/todo.js
@@ -22,4 +22,45 @@ $(document)
         }
 
         // code to be implemented
+
+        $(document).on('click', 'input[name=done-todo]', function (event) {
+             $(this)
+             .parent()
+             .toggleClass('checked');
+             }
+        );
+
+        function addTask(){
+            var generatedUUID = generateUUID();
+            var task = $('input[name=ListItem]').val();
+            $('input[name=ListItem]').val("");
+             $('ol').append('<li id=' + generatedUUID + ' class="">' +
+                '<input name="done-todo" type="checkbox" class="done-todo">' +
+                task + '</li>');
+            console.log(tasksList);
+        }
+        $('#button').click(addTask);
+
+        $('#filters li a').on('click',function(e){
+        $('#filters').find('a').removeClass('selected');
+                    $(this).addClass('selected');
+            var classStatus = $(this).text().toLowerCase();
+            var completedList = $('ol').find('li');
+
+             switch(classStatus){
+                case "all":
+                   completedList.show();
+                   break;
+
+                case "active":
+                    completedList.hide();
+                    completedList.not('.'+('checked')).show();
+                    break;
+
+                case "complete":
+                    completedList.show();
+                    completedList.not('.'+('checked')).hide();
+                    break;
+            }
+        })
     });
